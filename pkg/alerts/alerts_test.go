1f499d5218a070dce1c6b6524e48f040102bb6ec
Giedrius Statkevičius <giedriuswork@gmail.com>
2018-12-17 22:36:15 +0200

    alerts: move to using assert for tests

--- pkg/alerts/alerts_test.go
+++ pkg/alerts/alerts_test.go
@@ -32,30 +32,13 @@ func TestProcess(t *testing.T) {
 
 	s := NewState()
 	for _, v := range data {
-		err := s.AddAlert(v)
-		if err != nil {
-			t.Fatalf("failed to add alert: %v", err)
-		}
+		assert.Nil(t, s.AddAlert(v), "should have succeeded")
 	}
 
-	if s.Alerts[data[1].Hash()].Related[data[0].Hash()] != 1 {
-		t.Fatalf("failed to parse related data: 2nd alert has to be related to the 1st one (got %v)",
-			s.Alerts[data[1].Hash()].Related[data[0].Hash()])
-	}
-
-	if s.Alerts[data[0].Hash()].Related[data[1].Hash()] == 1 {
-		t.Fatalf("failed to parse related data: 1st alert must not be related to the 2nd one (got %v)",
-			data[0].Related[data[1].Hash()])
-	}
-
-	if len(s.Alerts[data[2].Hash()].Related) != 0 {
-		t.Fatalf("failed to parse related data: 2nd alert must not be related at all (got %v)",
-			len(s.Alerts[data[2].Hash()].Related))
-	}
-
-	if len(s.Firing) != 1 {
-		t.Fatalf("failed to parse related data: one alert must still be firing (got %v)", len(s.Firing))
-	}
+	assert.Contains(t, s.Alerts[data[1].Hash()].Related, data[0].Hash(), "2nd alert has to be related to the 1st one")
+	assert.NotContains(t, s.Alerts[data[0].Hash()].Related, data[1].Hash(), "1st alert must not be related to the 2nd one")
+	assert.Empty(t, s.Alerts[data[2].Hash()].Related, "must not be related to any other alerts")
+	assert.Len(t, s.Firing, 1, "one alert must be firing")
 }
 
 // TestCollision tests the case when two exact alerts are firing/resolved which is invalid.
@@ -85,28 +68,14 @@ func TestCollision(t *testing.T) {
 	}
 
 	s := NewState()
-	s.AddAlert(data[0])
-	err := s.AddAlert(data[1])
-	if err == nil {
-		t.Fatalf("adding the same alert did not return an error")
-	}
-
-	if len(data[0].Related) != 0 {
-		t.Fatalf("1st alert is related to something (got %v, expected %v)",
-			len(data[0].Related), 0)
-	}
-
-	if len(data[1].Related) != 0 {
-		t.Fatalf("2nd alert is related to something (got %v, expected %v)",
-			len(data[1].Related), 0)
-	}
-
+	assert.Nil(t, s.AddAlert(data[0]), "adding alert must not error")
+	assert.NotNil(t, s.AddAlert(data[1]), "adding same alert must return an error")
+	assert.Len(t, data[0].Related, 0, "1st alert must not be related to the 2nd one")
+	assert.Len(t, data[1].Related, 0, "2nd alert must not be related to the 1nd one")
 	assert.Equal(t, len(s.Firing), 1, "one alert should be firing")
 
-	err = s.AddAlert(data[2])
-	assert.Nil(t, err, "returned an error when it should not have")
-	assert.Lenf(t, s.Firing, 0, "no alerts should be firing")
+	assert.Nil(t, s.AddAlert(data[2]), "returned an error when it should not have")
+	assert.Len(t, s.Firing, 0, "no alerts should be firing")
 
-	err = s.AddAlert(data[3])
-	assert.NotNil(t, err, "should have returned an error")
+	assert.NotNil(t, s.AddAlert(data[3]), "should have returned an error")
 }
