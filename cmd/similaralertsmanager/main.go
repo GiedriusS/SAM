fdbfb54033c11556cf83c4547f82184ad3794875
Giedrius Statkevičius <giedriuswork@gmail.com>
2018-12-18 00:24:50 +0200

    cmd/SAM: add initial implementation

--- cmd/similaralertsmanager/main.go
+++ cmd/similaralertsmanager/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync"
+	"time"
 
 	"github.com/GiedriusS/SAM/pkg/alerts"
 	"github.com/GiedriusS/SAM/pkg/api"
@@ -46,11 +49,18 @@ func runSAM(l *zap.Logger, r *redis.Client, e *elastic.Client, addr *string) {
 	if err != nil {
 		l.Fatal("failed to initialize new Redis cache", zap.Error(err))
 	}
+	esSource, err := alerts.NewElasticSearchSource(fmt.Sprintf("alertmanager-%s", time.Now().Format("2006.01")),
+		e, l)
+	if err != nil {
+		l.Fatal("failed to initialize new ElasticSearch source", zap.Error(err))
+	}
+
 	state := alerts.NewState()
 	newState, err := rcache.GetState()
 	if err != nil {
 		l.Info("failed to get cache from Redis", zap.Error(err))
 	} else {
+		l.Info("got cache from Redis", zap.Time("last updated", newState.GetLastUpdated()))
 		state = newState
 	}
 
@@ -59,5 +69,50 @@ func runSAM(l *zap.Logger, r *redis.Client, e *elastic.Client, addr *string) {
 		Handler: api.R,
 		Addr:    *addr,
 	}
+
+	stateLock := sync.Mutex{}
+
+	go func() {
+		for {
+			select {
+			case <-time.After(15 * time.Second):
+			default:
+			}
+			stateLock.Lock()
+			defer stateLock.Unlock()
+
+			l.Info("getting alerts", zap.Time("from", state.GetLastUpdated()), zap.Time("to", time.Now()))
+			alerts, err := esSource.GetAlertsFromTo(state.GetLastUpdated(), time.Now())
+			if err != nil {
+				l.Error("failed to get alerts", zap.Error(err))
+				continue
+			}
+			for _, a := range alerts {
+				err := state.AddAlert(a)
+				if err != nil {
+					l.Error("failed to add alert", zap.Error(err))
+				}
+			}
+		}
+	}()
+
+	go func() {
+		for {
+			select {
+			case <-time.After(30 * time.Second):
+			default:
+			}
+			stateLock.Lock()
+			defer stateLock.Unlock()
+
+			l.Info("putting state")
+			err := rcache.PutState(&state)
+			if err != nil {
+				l.Error("failed to put cache", zap.Error(err))
+				continue
+			}
+		}
+	}()
+
 	l.Fatal("failed to listen", zap.Error(srv.ListenAndServe()))
 }
